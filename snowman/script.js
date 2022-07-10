fa73dc4ac3957ff038b304712c1b931c2adeaba7
unknown <salimiyahya549@gmail.com>
2022-07-10 23:38:37 +0300

    clean code

--- snowman/script.js
+++ snowman/script.js
@@ -1,19 +1,16 @@
-const topEyeLids = document.querySelectorAll(".eye-lid.top");
-const bottomEyeLids = document.querySelectorAll(".eye-lid.bottom");
-topEyeLids.forEach((el) => {
-	setInterval(() => {
-		el.style.top = "0";
-		setTimeout(() => {
-			el.style.top = "-10px";
-		}, 200);
-	}, 4000);
-});
+const upperEyeLids = document.querySelectorAll(".eye-lid.upper");
+const lowerEyeLids = document.querySelectorAll(".eye-lid.lower");
 
-bottomEyeLids.forEach((el) => {
-	setInterval(() => {
-		el.style.bottom = "0";
-		setTimeout(() => {
-			el.style.bottom = "-10px";
-		}, 200);
-	}, 4000);
-});
+function setBlinking(eyeLids, property) {
+	eyeLids.forEach((el) => {
+		setInterval(() => {
+			el.style[property] = "0";
+			setTimeout(() => {
+				el.style[property] = "-10px";
+			}, 200);
+		}, 4000);
+	});
+}
+
+setBlinking(upperEyeLids, "top");
+setBlinking(lowerEyeLids, "bottom");
